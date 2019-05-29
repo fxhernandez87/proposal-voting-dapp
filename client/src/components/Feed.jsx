88d2902354def692a6b4427004f698265f68e107
fxhernandez87 <fxhernandez87@gmail.com>
2019-05-29 14:19:48 -0300

    refactor component to unsubscribe on unmount component

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import PropTypes from "prop-types";
 import ThumbsUp from '../assets/ThumbsUp.svg';
 import ThumbsDown from '../assets/ThumbsDown.svg';
@@ -6,32 +6,39 @@ import ThumbsDown from '../assets/ThumbsDown.svg';
 import VotingContext from '../contexts/votingProposal';
 
 const Feed = ({ contract }) => {
+
+  const [eventEmit, setEventEmit] = useState(null);
   const context = useContext(VotingContext);
   const { voter: {address}, messages } = context;
 
-  const subscribeToVoteEvents = async () => {
-    contract.events.VoteReceived({fromBlock: 'latest', toBlock: 'latest'}).on('data', result => {
-      const {returnValues: {_from, _vote, _totalVotes}} = result;
-      context.addMessage({_from, _vote, _totalVotes});
-      if (context.voter.address.toUpperCase() === _from.toUpperCase() && !context.voter.hasVoted) {
-        context.setVoter({
-          ...context.voter,
-          vote: _vote,
-          hasVoted: true
-        });
-      }
-    }).on('error', err => {
-      context.setError(err);
-    });
+  const onData = result => {
+    const {returnValues: {_from, _vote, _totalVotes}} = result;
+    context.addMessage({_from, _vote, _totalVotes});
   };
 
-
+  const subscribeToVoteEvents = async (contract, address) => {
+    if (contract && address) {
+      setEventEmit(contract.events.VoteReceived({fromBlock: '0', toBlock: 'latest'}));
+    }
+  };
 
   useEffect(() => {
-    if (contract && address) {
-      subscribeToVoteEvents();
-    }
+      subscribeToVoteEvents(contract, address);
   }, [contract, address]);
+
+  useEffect(() => {
+    if (eventEmit) {
+      eventEmit
+        .on('data', onData)
+        .on('error', err => {
+          context.setError(err);
+        });
+
+      return async () => {
+        await eventEmit.unsubscribe();
+      }
+    }
+  }, [eventEmit]);
 
   const messagesStack = messages.reverse().map(ev => {
     const icon = ev._vote === 1 ? ThumbsUp : ThumbsDown;
